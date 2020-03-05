7893b05b72cc3aa42b8893cf7eca1382d860ec5f
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2020-03-05 21:57:39 +0000

    Fake bank simulator

---
+++ BankSimulator/src/FakeBank/Controllers/RetrieveController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeBank.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FakeBank.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RetrieveController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Post([FromBody]RetrieveModel retrieveModel)
+        {
+            if (DateTime.Today > 
+                new DateTime(retrieveModel.CardExpireYear, retrieveModel.CardExpireMonth, 
+                    DateTime.Today.Day))
+                return BadRequest("EXPIRED");
+
+            if(retrieveModel.CardNumber == 99999)
+                return BadRequest("INVALID");
+
+            return Ok(new RetrieveResponseModel()
+            {
+                Id = Guid.NewGuid()
+            });
+        }
+    }
+}
---
+++ BankSimulator/src/FakeBank/Models/RetrieveModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeBank.Models
+{
+    public class RetrieveModel
+    {
+        public decimal Amount { get; set; }
+        public string CardName { get; set; }
+        public long CardNumber { get; set; }
+        public int CardExpireMonth { get; set; }
+        public int CardExpireYear { get; set; }
+        public int CardCVV { get; set; }
+    }
+}
